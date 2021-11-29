5de729bc8a042b29b1887abe78505502af05350c
Stefanos Touf <stefetoufe@gmail.com>
2021-11-29 16:52:50 +0200

    Trigger and procedure planning

--- src/main/resources/database/create_entities.sql
+++ src/main/resources/database/create_entities.sql
@@ -1,9 +1,10 @@
 CREATE TABLE Bookings
 (
-    id            INT     GENERATED ALWAYS AS IDENTITY,
-    table_id      INT     NOT NULL, 
-    date          DATE    NOT NULL,
-    customer_name VARCHAR NOT NULL,
+    id             INT     GENERATED ALWAYS AS IDENTITY,
+    table_id       INT     NOT NULL,
+    date           DATE    NOT NULL,
+    customer_name  VARCHAR NOT NULL,
+    customer_count INT     NOT NULL,
     PRIMARY KEY(id),
     FOREIGN KEY(table_id)
       REFERENCES Tables(id)
@@ -21,7 +22,7 @@ CREATE TABLE Orders
 CREATE TABLE Menu_items 
 (
     id           INT     GENERATED ALWAYS AS IDENTITY,
-    name         VARCHAR NOT NULL,
+    name         VARCHAR NOT NULL UNIQUE,
     description  VARCHAR NOT NULL,
     price        REAL    NOT NULL,
     stock_number INT     NOT NULL,
@@ -45,5 +46,25 @@ CREATE TABLE Order_menu_item
       REFERENCES Tables(id),
   FOREIGN KEY(menu_item_id)
       REFERENCES Menu_items(id)
-
 )
+
+/*
+PLANNING:
+    PROCEDURES:
+        BOOKING:
+            create(table_id, date, customer_name, customer_count): id
+                if table is !available error else if customer_count < table_capacity insert else error
+        ORDERS:
+            create(table_id, menu_item_ids[]): id
+                if table is available error else if menu_items are available insert else error
+            get_price(menu_item_ids[]): price
+                sum price of all menu items
+        MENU_ITEM:
+            create(name, description, price, stock_num): id
+                creates menu item
+    TRIGGERS:
+        BOOKING:
+            ON CREATE SET REFERENCED TABLE UNAVAILABLE
+        ORDER:
+            ON CREATE DECREASE ALL STOCK NUMS OF REFERENCED MENU ITEMS
+*/

41734ca35bb9295efdd9ad5cee4c3d2fc6efb345
StefanosTouf <stefetoufe@gmail.com>
2021-11-29 14:53:27 +0200

    Create entities sql

---
+++ src/main/resources/database/create_entities.sql
@@ -0,0 +1,46 @@
+CREATE TABLE Bookings
+(
+    id            INT     GENERATED ALWAYS AS IDENTITY,
+    table_id      INT     NOT NULL, 
+    date          DATE    NOT NULL,
+    customer_name VARCHAR NOT NULL,
+    PRIMARY KEY(id),
+    CONSTRAINT fk_table
+      FOREIGN KEY(table_id)
+        REFERENCES Tables(id)
+);
+
+CREATE TABLE Orders 
+(
+    id            INT GENERATED ALWAYS AS IDENTITY,
+    table_id      INT NOT NULL, 
+    PRIMARY KEY(id),
+    CONSTRAINT fk_table
+      FOREIGN KEY(table_id)
+        REFERENCES Tables(id)
+);
+
+CREATE TABLE Menu_items 
+(
+    id           INT     GENERATED ALWAYS AS IDENTITY,
+    name         VARCHAR NOT NULL,
+    description  VARCHAR NOT NULL,
+    price        REAL    NOT NULL,
+    stock_number INT     NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE Tables
+(
+    id           INT     GENERATED ALWAYS AS IDENTITY,
+    capacity     INT     NOT NULL,
+    is_available BOOLEAN NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE Order_menu_item
+(
+  order_id     INT NOT NULL,
+  menu_item_id INT NOT NULL,
+  PRIMARY KEY (order_id, menu_item_id)
+)

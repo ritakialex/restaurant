f5c577184c694e58ca2d3a8e0c6c30719ac43936
Stefanos Touf <stefetoufe@gmail.com>
2021-12-01 14:48:11 +0200

    Decrement stock trigger

--- src/main/resources/database/create_triggers.sql
+++ src/main/resources/database/create_triggers.sql
@@ -9,6 +9,21 @@ BEGIN
 END; $$
 LANGUAGE plpgsql;
 
-CREATE TRIGGER employee_delete_trigger AFTER INSERT
-ON Orders FOR EACH ROW
+CREATE TRIGGER set_unavailable_trigger AFTER INSERT
+ON Bookings FOR EACH ROW
 EXECUTE PROCEDURE set_unavailable();
+
+CREATE OR REPLACE FUNCTION decrement_stock() RETURNS TRIGGER
+AS $$
+DECLARE
+BEGIN
+    UPDATE Menu_items AS m
+    SET  stock_number = stock_number - 1
+    WHERE m.id = NEW.menu_item_id;
+    RETURN New;
+END; $$
+    LANGUAGE plpgsql;
+
+CREATE TRIGGER set_unavailable_trigger AFTER INSERT
+    ON Order_menu_item FOR EACH ROW
+EXECUTE PROCEDURE decrement_stock();

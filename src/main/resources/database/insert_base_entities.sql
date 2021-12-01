cf4170876ce353f65e6c01fc19c65cae07206018
Stefanos Touf <stefetoufe@gmail.com>
2021-12-01 14:35:22 +0200

    Base static entities

---
+++ src/main/resources/database/insert_base_entities.sql
@@ -0,0 +1,58 @@
+INSERT INTO Menu_items(name, description, price, stock_number)
+VALUES ('Hamdog',
+        'If a hot dog and a burger had a child. A very ugly child.',
+        4.99, 25);
+
+INSERT INTO Menu_items(name, description, price, stock_number)
+VALUES ('Dry Chicken Noodle Soup',
+        'You get some powder, a glass of water and a pinch of salt. You do the rest',
+        6.99, 20);
+
+INSERT INTO Menu_items(name, description, price, stock_number)
+VALUES ('Existentialism Sandwich',
+        'A plate of nothing, or perhaps... everything? You can forge your own reality',
+        5.99, 10);
+
+INSERT INTO Menu_items(name, description, price, stock_number)
+VALUES ('The Optimist',
+        'A half full glass of spring water.',
+        0.5, 100);
+
+INSERT INTO Menu_items(name, description, price, stock_number)
+VALUES ('The Pessimist',
+        'A half empty glass of spring water, you debbie downer.',
+        0.5, 100);
+
+INSERT INTO Menu_items(name, description, price, stock_number)
+VALUES ('π-zza',
+        'A delicious theorem prover kit, complete with the toppings of your choice',
+        9.99, 25);
+
+INSERT INTO Menu_items(name, description, price, stock_number)
+VALUES ('D-I-Why?','You get a chance to join the chef and make anything you want, however you want it',
+        21.99, 25);
+
+INSERT INTO Menu_items(name, description, price, stock_number)
+VALUES ('Caligula''s Salad',
+        'Named after the lesser known, but equally important Roman emperor.',
+        4.99, 25);
+
+INSERT INTO Tables(capacity, is_available) VALUES (4, True);
+
+INSERT INTO Tables(capacity, is_available) VALUES (5, True);
+
+INSERT INTO Tables(capacity, is_available) VALUES (5, True);
+
+INSERT INTO Tables(capacity, is_available) VALUES (6, True);
+
+INSERT INTO Tables(capacity, is_available) VALUES (8, True);
+
+INSERT INTO Tables(capacity, is_available) VALUES (2, True);
+
+INSERT INTO Tables(capacity, is_available) VALUES (4, True);
+
+INSERT INTO Tables(capacity, is_available) VALUES (4, True);
+
+INSERT INTO Tables(capacity, is_available) VALUES (4, True);
+
+INSERT INTO Tables(capacity, is_available) VALUES (6, True);

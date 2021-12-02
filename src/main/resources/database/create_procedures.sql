79afc4932f0f83ce77f8d7479588cb2df1f4bf94
Stefanos Touf <stefetoufe@gmail.com>
2021-12-02 17:33:09 +0200

    Total price function

--- src/main/resources/database/create_procedures.sql
+++ src/main/resources/database/create_procedures.sql
@@ -1,5 +1,4 @@
 /*
-PLANNING:
     PROCEDURES:
         BOOKING:
             create(table_id, date, customer_name, customer_count): id
@@ -13,32 +12,52 @@ PLANNING:
             create(name, description, price, stock_num): id
                 creates menu item
 */
+CREATE OR REPLACE FUNCTION total_price(t_menu_item_ids INT[]) RETURNS REAL
+    LANGUAGE plpgsql
+AS
+$$
+DECLARE
+    p_sum REAL := 0;
+    m_id  INT;
+BEGIN
+    FOREACH m_id IN ARRAY t_menu_item_ids
+        LOOP
+            SELECT m.price + p_sum FROM Menu_items m WHERE id = m_id INTO p_sum;
+        END LOOP;
+    RETURN p_sum;
+END
+$$;
+
+
+
 CREATE OR REPLACE PROCEDURE create_order(t_table_id INT, t_menu_item_ids INT[])
-LANGUAGE plpgsql
-AS $$
+    LANGUAGE plpgsql
+AS
+$$
 DECLARE
     t_is_available BOOLEAN;
-    t_stock INT;
-    new_order_id INT;
-    m_id INT;
+    t_stock        INT;
+    new_order_id   INT;
+    m_id           INT;
 BEGIN
     IF t_is_available THEN
         RAISE EXCEPTION 'Table is not occupied';
     ELSE
         INSERT INTO Orders(table_id) VALUES (t_table_id) RETURNING id INTO new_order_id;
 
-        FOREACH m_id IN ARRAY t_menu_item_ids LOOP
-            SELECT stock_number FROM Menu_items WHERE id = m_id INTO t_stock;
+        FOREACH m_id IN ARRAY t_menu_item_ids
+            LOOP
+                SELECT stock_number FROM Menu_items WHERE id = m_id INTO t_stock;
 
-            IF NOT t_stock > 0 THEN
-                RAISE EXCEPTION 'Item is not in stock: %', m_id;
-            ELSE
-                INSERT INTO Order_menu_item(order_id, menu_item_id) VALUES(new_order_id, m_id);
-            END IF;
-        END LOOP;
+                IF NOT t_stock > 0 THEN
+                    RAISE EXCEPTION 'Item is not in stock: %', m_id;
+                ELSE
+                    INSERT INTO Order_menu_item(order_id, menu_item_id) VALUES (new_order_id, m_id);
+                END IF;
+            END LOOP;
     END IF;
-END $$;
-
+END
+$$;
 
 
 
@@ -46,11 +65,12 @@ CREATE OR REPLACE PROCEDURE create_booking(t_table_id INT,
                                            t_date DATE,
                                            t_customer_name VARCHAR,
                                            t_customer_count INT)
-LANGUAGE plpgsql
-AS $$
+    LANGUAGE plpgsql
+AS
+$$
 DECLARE
     t_is_available BOOLEAN;
-    t_capacity INT;
+    t_capacity     INT;
 BEGIN
     SELECT is_available, capacity
     FROM Tables
@@ -66,4 +86,5 @@ BEGIN
         VALUES (t_table_id, t_date, t_customer_name, t_customer_count);
     END IF;
 
-END $$;
+END
+$$;

3b0a04a9371d9862abc4eb1d98669dbe31a8c41b
Stefanos Touf <stefetoufe@gmail.com>
2021-12-23 19:15:07 +0200

    get with filter functions

--- src/main/resources/database/create_functions.sql
+++ src/main/resources/database/create_functions.sql
@@ -29,13 +29,92 @@ AS
 $$
 DECLARE
     item_ids INT[];
-    r_order ORDER_WITH_ITEMS;
+    r_order  ORDER_WITH_ITEMS;
 BEGIN
-    SELECT array_agg(id) FROM Order_menu_item o
-    WHERE o.order_id = t_order_id INTO item_ids;
+    SELECT array_agg(id)
+    FROM Order_menu_item o
+    WHERE o.order_id = t_order_id
+    INTO item_ids;
 
     SELECT *, item_ids FROM Orders o WHERE o.id = t_order_id INTO r_order;
 
     RETURN r_order;
+END
+$$;
+
+
+
+CREATE OR REPLACE FUNCTION get_bookings(t_table_id INT,
+                                        t_date DATE,
+                                        t_customer_name VARCHAR,
+                                        t_customer_count INT)
+    RETURNS SETOF BOOKINGS
+    LANGUAGE plpgsql
+AS
+$$
+DECLARE
+    where_q         VARCHAR := 'WHERE';
+    and_q           VARCHAR := '';
+    table_condition VARCHAR := '';
+    date_condition  VARCHAR := '';
+    name_condition  VARCHAR := '';
+    count_condition VARCHAR := '';
+BEGIN
+    IF t_table_id IS NOT NULL THEN
+        table_condition := format('%s %s table_id = %s', where_q, and_q, t_table_id);
+        and_q := 'AND';
+        where_q := '';
+    END IF;
+    IF t_date IS NOT NULL THEN
+        date_condition := format('%s %s date = ''%s''::DATE', where_q, and_q, t_date);
+        and_q := 'AND';
+        where_q := '';
+    END IF;
+    IF t_customer_name IS NOT NULL THEN
+        name_condition := format('%s %s customer_name = ''%s''', where_q, and_q, t_customer_name);
+        and_q := 'AND';
+        where_q := '';
+    END IF;
+    IF t_customer_count IS NOT NULL THEN
+        count_condition := format('%s %s customer_count = %s', where_q, and_q, t_customer_count);
+        and_q := 'AND';
+        where_q := '';
+    END IF;
+
+
+    RETURN QUERY EXECUTE format('SELECT * FROM BOOKINGS %s %s %s %s',
+                                table_condition,
+                                date_condition,
+                                name_condition,
+                                count_condition);
+END
+$$;
+
+CREATE OR REPLACE FUNCTION get_menu_items(t_food_category FOOD_CATEGORY,
+                                          t_stock_number INT)
+    RETURNS SETOF MENU_ITEMS
+    LANGUAGE plpgsql
+AS
+$$
+DECLARE
+    where_q            VARCHAR := 'WHERE';
+    and_q              VARCHAR := '';
+    category_condition VARCHAR := '';
+    stock_condition    VARCHAR := '';
+BEGIN
+    IF t_food_category IS NOT NULL THEN
+        category_condition := format('%s %s category = ''%s''', where_q, and_q, t_food_category);
+        and_q := 'AND';
+        where_q := '';
+    END IF;
+    IF t_stock_number IS NOT NULL THEN
+        stock_condition := format('%s %s stock_number >= %s', where_q, and_q, t_stock_number);
+        and_q := 'AND';
+        where_q := '';
+    END IF;
+
+    RETURN QUERY EXECUTE format('SELECT * FROM MENU_ITEMS %s %s',
+                                category_condition,
+                                stock_condition);
 END
 $$;

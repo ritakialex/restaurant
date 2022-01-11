cbfd57f9e770e6b8278f8614e9e631a45f89b153
StefanosTouf <stefetoufe@gmail.com>
2022-01-11 22:39:59 +0200

    prettify sample ops

--- src/main/resources/database/sample-ops.sql
+++ src/main/resources/database/sample-ops.sql
@@ -1,68 +1,70 @@
--- total_price(menu_item_ids INT[]) RETURNS REAL
-SELECT * FROM total_price(array[1,2,3,4,5,6])
+-- #Orders --
+SELECT total_price_order(1) --------------- total_price_order(order_id INT) 
+                                   -------- RETURNS REAL
 
--- total_price_order(order_id INT) RETURNS REAL
-SELECT * FROM total_price_order(1) 
+call create_order(7, array[1,3,4]) -------- create_order(table_id INT,
+                                   --------              menu_item_ids INT[],
+                                   --------              booking_id INT)
+                                   
+                                   -------- create_order(table_id INT,
+                                   --------              menu_item_ids INT[])
 
--- get_order_with_items(order_id INT) RETURNS SETOF ORDER_WITH_ITEMS
-SELECT * FROM get_order_with_items(1) 
+SELECT * FROM get_order_with_items(NULL) -- get_order_with_items(order_id INT) 
+                                         -- RETURNS SETOF ORDER_WITH_ITEMS
 
--- FUNCTION get_bookings(table_id INT,
---                       date DATE,
---                       customer_name VARCHAR,
---                       customer_count INT) RETURNS SETOF BOOKINGS
-SELECT * FROM get_bookings(NULL, NULL, NULL, NULL) 
-SELECT * FROM get_bookings(NULL, NULL, 'John F. Kennedy', NULL) 
+SELECT * FROM get_orders_logs() ----------- get_orders_logs() 
+                                ----------- RETURNS SETOF ORDERS_LOGS 
 
--- get_menu_items(food_category FOOD_CATEGORY,
---                min_stock_number INT) RETURNS SETOF MENU_ITEMS
-SELECT * FROM get_menu_items(NULL, NULL)
-SELECT * FROM get_menu_items('Appetizers', NULL) 
+-- #Bookings --
+SELECT * FROM get_bookings(NULL, NULL, NULL, NULL) -- FUNCTION get_bookings(table_id INT,
+                                                   --                       date DATE,
+                                                   --                       customer_name VARCHAR,
+                                                   --                       customer_count INT) 
+                                                   -- RETURNS SETOF BOOKINGS
 
--- get_tables(capacity INT,
---            is_available BOOLEAN) RETURNS SETOF TABLES
-SELECT * FROM get_tables(NULL, NULL) 
-SELECT * FROM get_tables(5, 'true') 
+call create_booking(1, 'yyyy-mm-dd', 1, '', 1) ------ create_booking(table_id INT,
+                                               ------                date DATE,
+                                               ------                hour INT,
+                                               ------                customer_name VARCHAR,
+                                               ------                customer_count INT)
 
--- toggle_availability(table_id INT)
-call toggle_availability(1)
+SELECT * FROM get_bookings_logs() ------------------- get_bookings_logs() 
+                                  ------------------- RETURNS SETOF BOOKINGS_LOGS 
+            
+call delete_booking(1) ------------------------------ delete_booking(booking_id INT)
 
--- create_menu_item(name VARCHAR,
---                  description VARCHAR,
---                  category FOOD_CATEGORY,
---                  price REAL,
---                  stock_number INT)
-call create_menu_item('name','a description','Appetizers',5.99,50)
 
--- create_order(table_id INT,
---              menu_item_ids INT[])
+-- #Menu_items --
+SELECT total_price(array[1,2,3,4,5,6])   -- total_price(menu_item_ids INT[]) 
+                                         -- RETURNS REAL
 
--- create_order(table_id INT,
---              menu_item_ids INT[],
---              booking_id INT)
-call create_order(3, array[1,2,3,4])
+SELECT * FROM get_menu_items(NULL, NULL) -- get_menu_items(food_category FOOD_CATEGORY,
+                                         --                min_stock_number INT) 
+                                         -- RETURNS SETOF MENU_ITEMS
 
--- create_booking(table_id INT,
---                date DATE,
---                hour INT,
---                customer_name VARCHAR,
---                customer_count INT)
-call create_booking(1, '2022-1-13', 16, 'Slim Shady', 4)
+call create_menu_item('', '', '', 1, 1) --- create_menu_item(name VARCHAR,
+                                        ---                  description VARCHAR,
+                                        ---                  category FOOD_CATEGORY,
+                                        ---                  price REAL,
+                                        ---                  stock_number INT)
 
--- delete_booking(booking_id INT)
-call delete_booking(1)
+call update_menu_item_price(1, -2.2) ------ update_menu_item_price(id INT, diff REAL)
 
--- update_menu_item_price(id INT, diff REAL)
-call update_menu_item_price(1, -2.2)
+call update_menu_item_stock(1, -10)  ------ update_menu_item_price(id INT, diff REAL)
 
--- update_menu_item_stock(id INT, diff INT)
-call update_menu_item_stock(1, 10)
+SELECT name, menu_item_id, operation, price, stock_number  FROM get_menu_items_logs() 
+                                     ------- get_bookings_logs() 
+                                     ------- RETURNS SETOF BOOKINGS_LOGS
 
--- logging
-SELECT * FROM get_tables_logs()
-SELECT * FROM get_bookings_logs()
-SELECT * FROM get_orders_logs()
-SELECT id, stamp, operation, userid, name, price, category FROM get_menu_items_logs()
+-- #Tables --
+SELECT * FROM get_tables(NULL, NULL) -- get_tables(capacity INT,
+                                     --            is_available BOOLEAN) 
+                                     -- RETURNS SETOF TABLES
+
+call toggle_availability(1) ----------- toggle_availability(table_id INT)
+
+SELECT * FROM get_tables_logs() ------- get_tables_logs()
+                                ------- RETURNS SETOF TABLES_LOGS
 
 
 

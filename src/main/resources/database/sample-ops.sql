48df5864f34186180f6937bad9ac737a25d2f5a7
StefanosTouf <stefetoufe@gmail.com>
2022-01-12 20:09:46 +0200

    streamline sample ops for presentation

--- src/main/resources/database/sample-ops.sql
+++ src/main/resources/database/sample-ops.sql
@@ -2,7 +2,7 @@
 SELECT total_price_order(1) --------------- total_price_order(order_id INT) 
                             --------------- RETURNS REAL
 
-call create_order(7, array[1,3,4]) -------- create_order(table_id INT,
+call create_order(2, array[1,3,4]) -------- create_order(table_id INT,
                                    --------              menu_item_ids INT[],
                                    --------              booking_id INT)
                                    --------
@@ -50,15 +50,17 @@ SELECT * FROM get_menu_items(NULL, NULL) -- get_menu_items(food_category FOOD_CA
                                          --                min_stock_number INT) 
                                          -- RETURNS SETOF MENU_ITEMS
 
+SELECT id, name, description, category, price, stock_number as stock FROM get_menu_items(NULL, 10) 
+                                         
 call create_menu_item('', '', '', 1, 1) --- create_menu_item(name VARCHAR,
                                         ---                  description VARCHAR,
                                         ---                  category FOOD_CATEGORY,
                                         ---                  price REAL,
                                         ---                  stock_number INT)
 
-call update_menu_item_price(1, -2.2) ------ update_menu_item_price(id INT, diff REAL)
+call update_menu_item_price(4, 1.25) ------ update_menu_item_price(id INT, diff REAL)
 
-call update_menu_item_stock(1, -10)  ------ update_menu_item_price(id INT, diff REAL)
+call update_menu_item_stock(4, -10)  ------ update_menu_item_price(id INT, diff REAL)
 
 SELECT name, menu_item_id, operation, price, stock_number  FROM get_menu_items_logs() 
                                      ------- get_bookings_logs() 
